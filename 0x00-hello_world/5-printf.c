3fbdb210dd4baab463d4b352b1993701d869759d
whykay10 <adelekeyinka36@yahoo.com>
2023-02-18 03:05:25 -0800

    hdfg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - This is the main function
  *
- * Return: 0 when successful
+ * Return: 0 when successfull
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);    
 }	

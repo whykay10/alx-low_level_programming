2cb05004af794eba48c0d8f54e06fc02305e47e8
whykay10 <adelekeyinka36@yahoo.com>
2023-02-22 03:59:43 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is where the major function stays
+ *
+ * Return: Always 0 when true
+ */
+
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory for nested loops and functions
